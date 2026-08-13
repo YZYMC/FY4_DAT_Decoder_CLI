59a49c675adcfc49b5bcab8a75c3f8eac7dd28cb
ZiYuan Yang <yzymc@yzynetwork.org>
2026-08-13 17:57:49 +0800

    Add verbose option and clang formatter config

--- fy4dec.c
+++ fy4dec.c
@@ -15,19 +15,19 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #define _GNU_SOURCE
 
 #include <argp.h>
+#include <endian.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <endian.h>
 #include <string.h>
 
-#include <openssl/evp.h>
 #include <openssl/err.h>
+#include <openssl/evp.h>
 #include <openssl/provider.h>
 
 char *datin = NULL;
@@ -35,56 +35,61 @@ bool sfileout = 0;
 char fileout[128] = "decrypted.bin";
 uint8_t deskey[8];
 char *keyfilepath = NULL;
+bool verbose = 0;
 
-const char	*argp_program_version =
-    "fy4dec 1.0\n"
-    "Copyright 2026 ZiYuan Yang\n"
-    "This is free software; see the source for copying conditions.  There is NO\n"
-    "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
-const char	*argp_program_bug_address = "yzymc@yzynetwork.org";
+const char *argp_program_version = "fy4dec 1.0\n"
+                                   "Copyright 2026 ZiYuan Yang\n"
+                                   "This is free software; see the source for "
+                                   "copying conditions.  There is NO\n"
+                                   "warranty; not even for MERCHANTABILITY or "
+                                   "FITNESS FOR A PARTICULAR PURPOSE.\n";
+const char *argp_program_bug_address = "yzymc@yzynetwork.org";
 
-static struct argp_option options[] =
-{
-	{"output", 'o', "FILE", 0, "Output file path"},
-    { "key-file", 'k', "FILE", 0, "DES Key binary file input"},
-	{0}
-};
+static struct argp_option options[] = {
+    {"output", 'o', "FILE", 0, "Output file path"},
+    {"key-file", 'k', "FILE", 0, "DES Key binary file input"},
+    {"verbose", 'v', 0, 0, "Verbose"},
+    {0}};
 
-static char	doc[]	   = "fy4dec -- FY4 LRIT DAT file decoder";
-static char	args_doc[] = "DATFILE";
+static char doc[] = "fy4dec -- FY4 LRIT DAT file decoder";
+static char args_doc[] = "DATFILE";
 
 error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
-	switch (key)
+    switch (key)
     {
-        case 'o':
-            sfileout = 1;
-            strcpy(fileout, arg);
-            break;
-        
-        case 'k':
-            keyfilepath = arg;
-            break;
-        
-    	case ARGP_KEY_ARG:
-	    	if (state->arg_num >= 1)
-	    		argp_usage(state);
-	    	datin = arg;
-	    	break;
-        
-        case ARGP_KEY_END:
-		    if (state->arg_num < 1)
-			    argp_usage(state);
-		    break;
-        
-	    default:
-	    	return ARGP_ERR_UNKNOWN;
-	}
+    case 'o':
+        sfileout = 1;
+        strcpy(fileout, arg);
+        break;
 
-	return 0;
+    case 'k':
+        keyfilepath = arg;
+        break;
+
+    case 'v':
+        verbose = 1;
+        break;
+
+    case ARGP_KEY_ARG:
+        if (state->arg_num >= 1)
+            argp_usage(state);
+        datin = arg;
+        break;
+
+    case ARGP_KEY_END:
+        if (state->arg_num < 1)
+            argp_usage(state);
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
 }
 
-static struct argp argp = { options, parse_opt, args_doc, doc };
+static struct argp argp = {options, parse_opt, args_doc, doc};
 
 int main(int argc, char **argv)
 {
@@ -103,6 +108,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    if (verbose)
+        printf("INFO: DAT file loaded: %s\n", datin);
+
     FILE *keyfile = fopen(keyfilepath, "rb");
     if (keyfile == NULL)
     {
@@ -110,9 +118,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (fread(deskey, 1, sizeof(deskey), keyfile) != sizeof(deskey)) {
+    if (fread(deskey, 1, sizeof(deskey), keyfile) != sizeof(deskey))
+    {
         return 1;
     }
+
+    if (verbose)
+        printf("INFO: Key file loaded: %s\n", keyfilepath);
 
     fclose(keyfile);
 
@@ -121,6 +133,9 @@ int main(int argc, char **argv)
     fseek(datfile, 0x46, SEEK_SET);
     fread(dat_filename, 1, 106, datfile);
 
+    if (verbose)
+        printf("INFO: Filename: %s\n", dat_filename);
+
     uint64_t bit_length;
 
     fseek(datfile, 0x08, SEEK_SET);
@@ -128,6 +143,9 @@ int main(int argc, char **argv)
 
     bit_length = be64toh(bit_length);
     uint64_t byte_length = bit_length / 8;
+
+    if (verbose)
+        printf("INFO: Length: %" PRIu64 " bytes\n", byte_length);
 
     char filetype[4];
 
@@ -150,10 +168,13 @@ int main(int argc, char **argv)
 
     FILE *outfile = fopen(fileout, "wb");
 
+    if (verbose)
+        printf("INFO: Output filename: %s\n", fileout);
+
     fseek(datfile, 0xE6, SEEK_SET);
 
     uint8_t readbuf[4096];
-    uint8_t outbuf[4096+8];
+    uint8_t outbuf[4096 + 8];
     size_t n;
     int outlen;
 
@@ -194,11 +215,10 @@ int main(int argc, char **argv)
         decrypted_length += outlen;
     }
 
-    if (EVP_DecryptFinal_ex(ctx, outbuf, &outlen) != 1)
-    {
-        fprintf(stderr, "Decrypt final failed\n");
-        return 1;
-    }
+    EVP_DecryptFinal_ex(ctx, outbuf, &outlen);
+
+    if (verbose)
+        printf("INFO: Decrypted length: %" PRIu64 " bytes\n", decrypted_length);
 
     fwrite(outbuf, 1, outlen, outfile);
     decrypted_length += outlen;
@@ -212,10 +232,9 @@ int main(int argc, char **argv)
     if (byte_length != decrypted_length)
     {
         fprintf(stderr,
-            "Warning: plaintext length mismatch: "
-            "expected %" PRIu64 " bytes, got %" PRIu64 " bytes\n",
-            byte_length,
-            decrypted_length);
+                "WARN: plaintext length mismatch: "
+                "expected %" PRIu64 " bytes, got %" PRIu64 " bytes\n",
+                byte_length, decrypted_length);
     }
 
     return 0;
